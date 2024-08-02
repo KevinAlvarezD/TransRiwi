5c41472eaf1127ff87da7e68159c012b2a970256
KevinAlvarezA <kevinadozanam23@gmail.com>
2024-08-02 06:53:42 -0500

    feat: Se crea herencia de customer con user y se añade el constructor

--- Models/Customer.cs
+++ Models/Customer.cs
@@ -5,9 +5,16 @@ using System.Threading.Tasks;
 
 namespace TransRiwi.Models;
 
-public class Customer
+public class Customer : User
 {
     public string MembershipLevel { get; set; }
     public string PreferredPaymentMethod { get; set; }
+
+    //constructor
+    public Customer(string name, string lastName, string typeDocument, string identificationNumber, DateOnly birthDate, string email, string phoneNumber, string adreess, string membershipLevel, string preferredPaymentMethod) : base(name, lastName, typeDocument, identificationNumber, birthDate, email, phoneNumber, adreess)
+    {
+        MembershipLevel = membershipLevel;
+        PreferredPaymentMethod = preferredPaymentMethod;
+    }
 
 }
