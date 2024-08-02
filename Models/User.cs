b9cba181497ed4a05fe9943d1a9a3e09ad3c1f48
KevinAlvarezA <kevinadozanam23@gmail.com>
2024-08-02 06:43:21 -0500

    feat: Se crea la clase user

---
+++ Models/User.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TransRiwi.Models;
+
+public class User
+{
+    protected Guid Id { get; set; }
+    protected string Name { get; set; }
+    protected string LastName { get; set; }
+    protected string TypeDocument { get; set; }
+    protected string IdentificationNumber { get; set; }
+    protected DateOnly BirthDate { get; set; }
+    protected string Email { get; set; }
+    protected string PhoneNumber { get; set; }
+    protected string Adreess { get; set; }
+
+    //constructor
+    protected User(string name, string lastName, string typeDocument, string identificationNumber, DateOnly birthDate, string email, string phoneNumber, string adreess)
+    {
+        Id = Guid.NewGuid();
+        Name = name;
+        LastName = lastName;
+        TypeDocument = typeDocument;
+        IdentificationNumber = identificationNumber;
+        BirthDate = birthDate;
+        Email = email;
+        PhoneNumber = phoneNumber;
+        Adreess = adreess;
+    }
+    //metodos
+    //showDetails()void
+    //CalculateAge()int
+    //ShowAge()void
+}
