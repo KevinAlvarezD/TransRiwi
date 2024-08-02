d8f5d1cf23b2d57d7c0f4f7e45e104e834cd587b
KevinAlvarezA <kevinadozanam23@gmail.com>
2024-08-02 08:28:57 -0500

    fix: se agrega ver informacion en la clase user

--- Models/User.cs
+++ Models/User.cs
@@ -32,6 +32,44 @@ public class User
     }
     //metodos
     //showDetails()void
+    public virtual void ShowDetails()
+    {
+         Console.WriteLine("==============================================================================");
+            Console.WriteLine("                          INFORMACION PERSONAL                             ");
+            Console.WriteLine("===========================================================================");
+            Console.WriteLine("| {0,-1} | {1,-43} |", "ID                   |",$"{Id}");
+            Console.WriteLine("| {0,-1} | {1,-43} |", "Nombre               |",$"{Name}");
+            Console.WriteLine("| {0,-1} | {1,-43} |", "Apellido             |",$"{LastName}");
+            Console.WriteLine("| {0,-1} | {1,-43} |", "Tipo De Documento    |",$"{TypeDocument}");
+            Console.WriteLine("| {0,-1} | {1,-43} |", "Numero de Documento  |",$"{IdentificationNumber}");
+            Console.WriteLine("| {0,-1} | {1,-43} |", "Cumpleaños           |",$"{BirthDate}");
+            Console.WriteLine("| {0,-1} | {1,-43} |", "Email                |",$"{Email}");
+            Console.WriteLine("| {0,-1} | {1,-43} |", "Telefono             |",$"{PhoneNumber}");
+            Console.WriteLine("| {0,-1} | {1,-43} |", "Direccion            |",$"{Adreess}");
+    }
+
+
     //CalculateAge()int
+    public  int CalculateAge()
+    {
+        int age = DateTime.Today.Year - BirthDate.Year;
+        int month = DateTime.Today.Month - BirthDate.Month;
+        if (month < 0 || (month == 0 && DateTime.Today.Day < BirthDate.Day))
+        {
+            age--;
+        }
+        return age;
+    }
+
+
     //ShowAge()void
+    public  void ShowAge()
+    {
+        int age = CalculateAge();
+        Console.WriteLine("===========================================================================");
+        Console.WriteLine("                             INFORMACION DE EDAD                           ");
+        Console.WriteLine("===========================================================================");
+        Console.WriteLine($"| {Id,-1} | {age,-43} |                                                   ");
+        Console.WriteLine("===========================================================================");
+    }
 }
